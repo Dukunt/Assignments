9c17ddddc44a6a113d995579fcd29982897aba68
sateesh <sateesh1417@gmail.com>
2026-02-05 16:46:32 +0000

    message

--- 01.Findfile.sh
+++ 01.Findfile.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 FILE=$1
 find -iname $FILE
+
 if [ "$?" -eq 0 ]; then
     echo " File exist"
     echo "$?"
+    exit 1
     else
     echo "File Does not exist"
     echo "$?"
+    exit 1
 fi    
